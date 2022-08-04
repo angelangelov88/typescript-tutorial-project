462ac05aba67b2150ac76b6c552c523e621ed059
Angel <82048582+angelangelov88@users.noreply.github.com>
2022-08-04 11:35:38 +0100

    start files added

---
+++ sandbox.js
@@ -0,0 +1,3 @@
+var names = ['angel', 'luigi', 'mario'];
+names.push('toad');
+console.log(names);
---
+++ sandbox.ts
@@ -0,0 +1,5 @@
+let names = ['angel', 'luigi', 'mario'];
+
+names.push('toad');
+
+console.log(names);
